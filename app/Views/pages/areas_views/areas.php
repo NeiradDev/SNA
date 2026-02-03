fc3c7dcd381726895cbfc2ffca087759eb682827
local V <teslamanager@protonmail.com>
2026-02-03 16:09:06 -0500

    Cambio de DB se creo tabla division, se llama la tabla al crear usuario cambio de listar usuario refact

---
+++ app/Views/pages/areas_views/areas.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('titulo') ?>
+Bestpc SNA - Areas
+<?= $this->endSection() ?>
+<?= $this->section('contenido') ?>
+<div class="container-fluid">
+    <p>Hola</p>
+</div>
+<?= $this->endSection() ?>
